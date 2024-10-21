e7deb49d70d88a07fef6e472044473d5dc38c729
nautbek <nautbek@gmail.com>
2024-10-21 06:09:06 +0300

    Base welcome route.

--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
 
+func main() {
+	http.HandleFunc("/", home)
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatalf("Ошибка при запуске сервера %v", err)
+	}
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	_ = json.NewEncoder(w).Encode("Welcome")
 }
