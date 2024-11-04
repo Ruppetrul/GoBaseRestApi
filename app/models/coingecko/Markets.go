808a5f2d108daca443ee821ad73ea5f4ff844532
nautbek <nautbek@gmail.com>
2024-11-04 17:12:49 +0300

    Mini fixes.

--- app/models/coingecko/Markets.go
+++ app/models/coingecko/Markets.go
@@ -4,14 +4,40 @@ import (
 	"database/sql"
 	"firstRest/database"
 	"fmt"
+	"strconv"
+	"strings"
 )
 
 type Markets struct {
-	Id           string  `db:"id"`
-	Symbol       string  `db:"symbol"`
-	Name         string  `db:"name"`
-	CurrentPrice float32 `json:"current_price" db:"current_price"`
-	MarketCap    int64   `json:"market_cap" db:"market_cap"`
+	Id             string  `db:"id"`
+	Symbol         string  `db:"symbol"`
+	Name           string  `db:"name"`
+	CurrentPrice   float32 `json:"current_price" db:"current_price"`
+	FormattedPrice string
+	MarketCap      int64 `json:"market_cap" db:"market_cap"`
+	FormattedCap   string
+}
+
+func FormatPrice(price float32) string {
+	priceStr := strconv.FormatFloat(float64(price), 'f', -1, 32)
+
+	parts := strings.Split(priceStr, ".")
+
+	intPart := parts[0]
+	intPartWithCommas := ""
+
+	for i, digit := range intPart {
+		if i > 0 && (len(intPart)-i)%3 == 0 {
+			intPartWithCommas += "."
+		}
+		intPartWithCommas += string(digit)
+	}
+
+	if len(parts) == 1 {
+		return intPartWithCommas
+	}
+
+	return intPartWithCommas + "," + parts[1]
 }
 
 func (p *Markets) Save() (sql.Result, error) {
@@ -26,7 +52,10 @@ func (p *Markets) Save() (sql.Result, error) {
 }
 
 func GetList(orderField string) ([]Markets, error) {
-	rows, err := database.Select(fmt.Sprintf("SELECT id, symbol, name, current_price, market_cap FROM coingecko ORDER BY %s DESC;", orderField))
+	rows, err := database.Select(fmt.Sprintf("SELECT "+
+		"id, UPPER(symbol) as symbol, name,"+
+		" ROUND(current_price, 8) AS current_price, market_cap "+
+		"FROM coingecko ORDER BY %s DESC;", orderField))
 	if err != nil {
 		return nil, err
 	}
@@ -38,6 +67,8 @@ func GetList(orderField string) ([]Markets, error) {
 		if err := rows.Scan(&market.Id, &market.Symbol, &market.Name, &market.CurrentPrice, &market.MarketCap); err != nil {
 			return nil, err
 		}
+		market.FormattedPrice = FormatPrice(market.CurrentPrice)
+		market.FormattedCap = FormatPrice(float32(market.MarketCap))
 		markets = append(markets, market)
 	}
 
